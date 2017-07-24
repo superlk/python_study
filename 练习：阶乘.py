b0846cc7f848e18b322f2a735f7c975eafb2e811
superlk <651002081@qq.com>
2017-07-24 09:31:31 +0800

    Update 练习：阶乘.py

--- 练习：阶乘.py
+++ 练习：阶乘.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
 #_*_coding:utf-8_*_
-#½×³Ë¾ÍÊÇ6£¡=6*5*4*3*2*1
-'''
+#é˜¶ä¹˜å°±æ˜¯6ï¼=6*5*4*3*2*1
+
 def jiecheng(num):
     res=1
     for i in range(1,num+1):
         res *=i
     return res
-num=int(raw_input('ÇëÊäÈë½×³ËÊı×Ö£º'))
+num=int(raw_input('è¯·è¾“å…¥é˜¶ä¹˜æ•°å­—ï¼š'))
 jiecheng(num)
-'''
+
+#é€’å½’ç”¨æ³•
+
 #6!=6*5!  
 #1!=1
 def fib(num):
     if num==1:
         return 1
     return num*fib(num-1)
-num=int(raw_input('ÇëÊäÈë½×³ËÊı×Ö£º'))
+num=int(raw_input('è¯·è¾“å…¥é˜¶ä¹˜æ•°å­—ï¼š'))
 print fib(num)
